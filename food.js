05849ceb7e3436e893b31642431fd62a155b4c04
Justafigurehead <strachanmichaela@hotmail.com>
2017-03-22 15:54:43 +0000

    Added tests in food to test if it has a name and if it has replenish points.

---
+++ food.js
@@ -0,0 +1,6 @@
+var Food = function(name){
+  this.name = name;
+  this.replenish = 15;
+}
+
+module.exports = Food;
---
+++ specs/food_spec.js
@@ -0,0 +1,23 @@
+var Food = require('../food');
+var assert = require('assert');
+
+
+
+describe('Food', function(){
+
+  var rye;
+
+  beforeEach(function(){
+
+    rye = new Food("Conjured Rye");
+  });
+
+  it('should have a title', function(){
+    assert.equal('Conjured Rye', rye.name);
+  });
+
+  it('should have heal points attached', function(){
+    assert.equal(15, rye.replenish);
+  })
+
+});
